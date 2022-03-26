06b25d398a27b710de3f202756dfb7d76f7bdb1c
John Doe <johndoe@example.com>
2022-03-26 01:03:32 -0500

    Commit Implementation of Service and Repository GetCourses

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,29 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private ICourseRepository  _courseRepository;
+
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel()
+            {
+                Courses = _courseRepository.GetCourses()
+            };
+        }
+    }
+}
